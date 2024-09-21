4ae95a0dbac5871abbe5d5cd2899a3571d17cf71
Alex <smileyrec@gmail.com>
2024-09-21 23:02:59 +0200

    Added functionality for scraped data

--- src/scraping/scraping.service.ts
+++ src/scraping/scraping.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from "mongoose";
+import { InjectModel } from "@nestjs/mongoose";
+import puppeteer from 'puppeteer';
+import { Url, UrlDocument } from "../../schemas/url.schema";
 
 @Injectable()
 export class ScrapingService {
-  async scrapeUrl(url: string) {
-    return 'Hello World!';
+  constructor(@InjectModel(Url.name) private urlModel: Model<UrlDocument>) {}
+
+  async scrapeUrl(url: string): Promise<Url> {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    const title = await page.title();
+    const image = await page.$eval("img", (img: HTMLImageElement) => img.src);
+    const text = await page.$eval('article p', (p: HTMLParagraphElement) => p.innerText)
+
+    await browser.close();
+
+    const scrapedData = new this.urlModel({ url, title, image, text });
+    return scrapedData.save();
   }
 }
